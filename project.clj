a7ff82c93de2209baf391dbcecdbc412b2f419cb
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2014-09-03 19:00:48 +0100

    Added plugins

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
   :url "http://example.com/FIXME"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [
-                 [org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.neo4j/neo4j ~neo4j-version]
                  [org.neo4j/neo4j-kernel ~neo4j-version :classifier "tests"]
                  [org.neo4j/server-api ~neo4j-version]
@@ -14,6 +13,10 @@
                  [org.neo4j.app/neo4j-server ~neo4j-version :classifier "tests"]
                  [com.sun.jersey/jersey-client "1.9"]
                  [junit/junit "4.11"]]
+  :plugins [[lein-cloverage "1.0.2"]
+            [lein-ancient "0.5.5"]
+            [jonase/eastwood "0.1.4"]
+            [lein-kibit "0.0.8"]]
   :main ^:skip-aot clj-archetype.core
   :target-path "target/%s"
   :aot :all
